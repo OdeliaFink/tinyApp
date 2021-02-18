af7833f714e184592c12f68f2fef844c1fa6c337
OdeliaFink <odelia.finkel@gmail.com>
2021-02-18 18:40:48 +0000

    Emptied User DB Object

--- express_server.js
+++ express_server.js
@@ -57,18 +57,7 @@ const urlDatabase = {
 };
 
 //user database
-const users = {
-  "userRandomID": {
-    id: "userRandomID",
-    email: "user@example.com",
-    password: "purple-monkey-dinosaur"
-  },
-  "user2RandomID": {
-    id: "user2RandomID",
-    email: "user2@example.com",
-    password: "dishwasher-funk"
-  }
-};
+const users = {};
 
 //GET request for the homepage
 app.get("/", (req, res) => {
